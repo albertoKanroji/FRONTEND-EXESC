b2fb4ac55fc7f037d872f65f3253345166f93a5c
albertoKanroji <albert.hdz855@gmail.com>
2024-06-22 22:58:37 -0600

    x

--- src/app/demo/components/uikit/grupos/grupos.component.ts
+++ src/app/demo/components/uikit/grupos/grupos.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { Actividad } from 'src/app/interfaces/actividades';
 import { GruposService } from 'src/app/services/grupos/grupos.service';
 
@@ -12,18 +13,23 @@ export class GruposComponent {
     actividades: Actividad[] = [];
     loading: boolean = true;
     @ViewChild('filter') filter!: ElementRef;
-    constructor(private actividadesService: GruposService) {}
+    constructor(
+        private actividadesService: GruposService,
+        private toastr: ToastrService
+    ) {}
 
     ngOnInit(): void {
         this.actividadesService.getGroups().subscribe(
             (data: any) => {
                 if (data.success) {
+                    this.showSuccess();
                     this.actividades = data.data;
                     console.log('Actividades:', this.actividades);
                 }
                 this.loading = false;
             },
             (error) => {
+                this.showError();
                 console.error('Error al obtener las actividades:', error);
                 this.loading = false;
             }
@@ -38,5 +44,11 @@ export class GruposComponent {
 
     clear(table: any) {
         table.clear();
+    }
+    showSuccess() {
+        this.toastr.info('Completado', 'Datos cargados');
+    }
+    showError() {
+        this.toastr.error('Error', 'Ocurrio un error');
     }
 }
